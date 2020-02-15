e8f84457c7845f641dea3d0a9b22e146a0f902e7
void255 <50835251+void255@users.noreply.github.com>
2020-02-15 15:43:36 +0200

    Update init.el

--- .emacd.d/init.el
+++ .emacd.d/init.el
@@ -39,24 +39,3 @@
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
- '(custom-safe-themes
-   (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1068ae7acf99967cc322831589497fee6fb430490147ca12ca7dd3e38d9b552a" default)))
- '(package-selected-packages
-   (quote
-    (color-theme-sanityinc-solarized zzz-to-char popup-kill-ring company flycheck htmlize ox-twbs zerodark-theme which-key use-package swiper spaceline rainbow-mode rainbow-delimiters projectile pretty-mode mark-multiple linum-relative hungry-delete expand-region dashboard avy async))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
