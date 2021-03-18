30cf1a07dc7acceeec750fae8def593754249609
void255 <50835251+void255@users.noreply.github.com>
2021-03-18 01:02:31 +0000

    Update packages.sh

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 # this install script assumes that you have already edited the parameters in /etc/pacman.conf to enable multilibs and testing, ect. 
 
+# root usage
+sudo su
+
 #optionally add support for aur 
 mkdir -p ~/Downloads
 cd $HOME/Downloads
-sudo pacman -S --noconfirm git
+pacman -S --noconfirm git
 git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si
-cd ~
 
 sleep 2
 
 # setting up pacman.conf
-sudo su
 cd $HOME/dots/etc
 mv pacman.conf /etc/pacman.conf
-exit
+cd
 
 sleep 2
 
 # (optional) install doas
-sudo pacman -S --noconfirm opendoas
+pacman -S --noconfirm opendoas
 cd $HOME/dots/etc
-sudo mv doas.conf /etc
-cd ~
+mv doas.conf /etc
 
 sleep 2
 
 # pulseaudio
-sudo pacman -S --noconfirm pulseaudio-alsa pulsemixer
+pacman -S --noconfirm pulseaudio-alsa pulsemixer
 #sudo systemctl emable pulseaudio
 #sudo systemctl start pulseaudio
-cd ~
 
 sleep 2
 
 # nvidia drivers (requires non-free and for steam support multilibs)
-sudo pacman -S --noconfirm nvidia-dkms nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils
-cd ~
+pacman -S --noconfirm nvidia-dkms nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils
 
 sleep 2
 
 # xorg
-sudo pacman -S --noconfirm xorg
-cd ~
+pacman -S --noconfirm xorg
