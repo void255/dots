d32e554cc1e361d00ff8cad1f0756fe350e5616d
void255 <50835251+void255@users.noreply.github.com>
2021-03-18 01:19:43 +0000

    Update packages.sh

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -4,8 +4,6 @@
 # root usage
 sudo su
 
-sleep 1
-
 # optionally add support for aur 
 mkdir -p ~/Downloads
 cd $HOME/Downloads
@@ -14,41 +12,27 @@ git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
 
-sleep 1
-
 # (optional) install doas
 pacman -S --noconfirm opendoas
 cd $HOME/dots/etc
 mv doas.conf /etc
-
-sleep 1
 
 # pulseaudio
 pacman -S --noconfirm pulseaudio-alsa pulsemixer
 #sudo systemctl emable pulseaudio
 #sudo systemctl start pulseaudio
 
-sleep 1
-
 # nvidia drivers (requires non-free and for steam support multilibs)
 pacman -S --noconfirm nvidia-dkms nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils
-
-sleep 1
 
 # xorg
 pacman -S --noconfirm xorg
 
-sleep 1
-
 # steam
 yay -S --noconfirm steam
 
-sleep 1
-
 # librewolf (mv the makepkg or change the makeopts yourself)
 yay -S --noconfirm librewolf
-
-sleep 1
 
 # runelite
 yay -S --noconfirm runelite
