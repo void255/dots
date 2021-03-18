b11a2d80420bc97ba76cbb7a11e94a15e0a280e3
void255 <50835251+void255@users.noreply.github.com>
2021-03-18 00:44:30 +0000

    Update packages.sh

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#this install script assumes that you have already edited the parameters in /etc/pacman.conf \
-to enable multilibs and testing, ect. 
+# this install script assumes that you have already edited the parameters in /etc/pacman.conf to enable multilibs and testing, ect. 
 
 #optionally add support for aur 
 mkdir -p ~/Downloads
@@ -15,3 +14,10 @@ sudo pacman -S --noconfirm opendoas
 cd $HOME/dots/etc
 sudo mv doas.conf /etc
 
+# pulseaudio
+sudo pacman -S --noconfirm pulseaudio-alsa pulsemixer
+#sudo systemctl emable pulseaudio
+#sudo systemctl start pulseaudio
+
+# nvidia drivers (requires non-free and for steam support multilibs)
+sudo pacman -S --noconfirm nvidia-dkms nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils
