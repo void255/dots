08a8cdb824b27f831aa0af898ae0a22e00f90deb
void255 <50835251+void255@users.noreply.github.com>
2021-03-18 00:59:19 +0000

    Update packages.sh

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -10,11 +10,15 @@ cd yay/
 makepkg -si
 cd ~
 
+sleep 2
+
 # setting up pacman.conf
 sudo su
 cd $HOME/dots/etc
 mv pacman.conf /etc/pacman.conf
 exit
+
+sleep 2
 
 # (optional) install doas
 sudo pacman -S --noconfirm opendoas
@@ -22,15 +26,21 @@ cd $HOME/dots/etc
 sudo mv doas.conf /etc
 cd ~
 
+sleep 2
+
 # pulseaudio
 sudo pacman -S --noconfirm pulseaudio-alsa pulsemixer
 #sudo systemctl emable pulseaudio
 #sudo systemctl start pulseaudio
 cd ~
 
+sleep 2
+
 # nvidia drivers (requires non-free and for steam support multilibs)
 sudo pacman -S --noconfirm nvidia-dkms nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils
 cd ~
+
+sleep 2
 
 # xorg
 sudo pacman -S --noconfirm xorg
