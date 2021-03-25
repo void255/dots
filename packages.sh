98746b49ffa3e13b85a258a5908976b3c9272874
void255 <50835251+void255@users.noreply.github.com>
2021-03-25 17:38:41 +0000

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# this install script assumes that you have already edited the parameters in /etc/pacman.conf to enable multilibs and testing, ect. 
+
+# pulseaudio
+#pacman -S --noconfirm pulseaudio-alsa pulsemixer
+#sudo systemctl emable pulseaudio
+#sudo systemctl start pulseaudio
+
+# xorg
+yay -S --noconfirm xorg-xinit xorg steam brave-bin runelite-launcher multimc5 scrot rofi i3 alsa-utils qbittorrent mpv deadbeef discord neofetch zathura neovim xdotool
