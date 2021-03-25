8f05de106fe9ece805f6a6bc87e7e2d9f358199c
void255 <50835251+void255@users.noreply.github.com>
2021-03-25 17:31:21 +0000

    Delete Scripts directory

--- Scripts/hosts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo su
-cd $HOME/Downloads
-git clone https://github.com/StevenBlack/hosts
-mv hosts/hosts /etc/hosts
--- Scripts/user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo useradd -m -G wheel,audio,video,network,input lain
-sudo passwd lain
