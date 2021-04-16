e3f9ef2b2368315846135bf08849ad38bcb4a181
void255 <50835251+void255@users.noreply.github.com>
2021-04-16 14:53:01 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 # this install script assumes that you have already edited the parameters in /etc/pacman.conf to enable multilibs and testing, ect. 
 
-# pulseaudio
-#pacman -S --noconfirm pulseaudio-alsa pulsemixer
-#sudo systemctl emable pulseaudio
-#sudo systemctl start pulseaudio
-
 # xorg
-yay -S --noconfirm xorg-xinit xorg steam brave-bin runelite multimc5 scrot rofi i3 \
+yay -S --noconfirm xorg-xinit xorg steam brave-bin runelite multimc5 scrot rofi i3-gaps \
 alsa-utils qbittorrent mpv deadbeef discord neofetch zathura neovim xdotool xbindkeys \
-alacritty ttf-anonymous-pro ttf-linux-libertine inter-font terminus-font ttf-hanazono \
-ttf-sazanami unzip lxappearance nautilus youtube-dl htop pulseaudio-alsa pulsemixer \
-sxiv \
+alacritty ttf-anonymous-pro ttf-linux-libertine ttf-liberation inter-font terminus-font \
+ttf-hanazono ttf-sazanami unzip lxappearance nautilus youtube-dl htop pulseaudio-alsa \ 
+pulsemixer sxiv dunst \
