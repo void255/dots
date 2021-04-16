2a3300a73e4727c6f1737f1a7d9a418cd680fe84
void255 <50835251+void255@users.noreply.github.com>
2021-04-16 14:53:56 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,8 +2,9 @@
 # this install script assumes that you have already edited the parameters in /etc/pacman.conf to enable multilibs and testing, ect. 
 
 # xorg
-yay -S --noconfirm xorg-xinit xorg steam brave-bin runelite multimc5 scrot rofi i3-gaps \
-alsa-utils qbittorrent mpv deadbeef discord neofetch zathura neovim xdotool xbindkeys \
-alacritty ttf-anonymous-pro ttf-linux-libertine ttf-liberation inter-font terminus-font \
-ttf-hanazono ttf-sazanami unzip lxappearance nautilus youtube-dl htop pulseaudio-alsa \ 
-pulsemixer sxiv dunst \
+yay -S --noconfirm dhcpcd-openrc xorg-xinit xorg steam brave-bin runelite multimc5 \
+scrot rofi i3-gaps pulsemixer sxiv dunst neofetch zathura neovim xdotool xbindkeys \
+alsa-utils qbittorrent mpv deadbeef discord ttf-liberation inter-font terminus-font \
+alacritty ttf-anonymous-pro ttf-linux-libertine youtube-dl htop pulseaudio-alsa \
+ttf-hanazono ttf-sazanami unzip lxappearance nautilus \
+
