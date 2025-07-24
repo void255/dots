0def515bc5faa20cc588db9f67c2ab7a4dca50cb
void255 <50835251+void255@users.noreply.github.com>
2025-07-24 13:15:28 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,9 +2,9 @@
 # this install script assumes that you have already edited the parameters in /etc/pacman.conf to enable multilibs and testing, ect. 
 
 # xorg
-yay -S --noconfirm dhcpcd xorg-xinit xorg steam ungoogled-chromium-bin lutris multimc5 \
-scrot rofi sxiv neofetch zathura neovim xdotool xbindkeys \
-qbittorrent mpv DeadBeef discord ttf-liberation inter-font terminus-font \
+yay -S --noconfirm dhcpcd network-manager xorg-xinit xorg-server xorg steam adwaita-steam-gtk \
+librewolf lutris multimc5 flameshot rofi feh fastfetch zathura vim xdotool sxhkd \
+qbittorrent mpv DeadBeef vesktop-bin ttf-liberation inter-font terminus-font \
 alacritty ttf-anonymous-pro ttf-linux-libertine yt-dlp htop pipewire wireplumber \
 ttf-hanazono ttf-sazanami unzip tar unrar 7-zip
 
