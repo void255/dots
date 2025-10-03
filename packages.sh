bd38f880ea18280c1ddea69b8abbac0654eeb63e
void255 <50835251+void255@users.noreply.github.com>
2025-10-03 18:40:21 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,5 +6,5 @@ yay -S --noconfirm dhcpcd network-manager xorg-xinit xorg-server xorg steam adwa
 librewolf lutris multimc5 flameshot rofi feh fastfetch zathura vim xdotool sxhkd \
 qbittorrent mpv DeadBeef vesktop-bin ttf-liberation inter-font terminus-font \
 alacritty ttf-anonymous-pro ttf-linux-libertine yt-dlp htop pipewire wireplumber \
-ttf-hanazono ttf-sazanami unzip tar unrar 7-zip
+ttf-hanazono ttf-sazanami unzip tar unrar 7-zip sddm swww swww-daemon hyprland \
 
